8619c5d2b771d2efeecefb41c70c462243b6b1cf
jackyang <jy6231@yahoo.com.tw>
2019-11-16 10:12:25 +0800

    Initial Commit

---
+++ uiview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiview
+//
+//  Created by MBP_JackYang on 2019/11/16.
+//  Copyright © 2019 MBP_JackYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
