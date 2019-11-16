9b4b81f10a12236eecfb47a002f0e32a57cde13b
jackyang <jy6231@yahoo.com.tw>
2019-11-16 11:21:37 +0800

    set timer

--- uiview/ViewController.swift
+++ uiview/ViewController.swift
@@ -11,13 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     var theView:UIView!
+    var timer:Timer!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         theView = UIView(frame:CGRect(x: 0, y: 0, width: 100, height: 100))
         theView.center = self.view.center
         theView.backgroundColor = UIColor.red
+        theView.transform = CGAffineTransform(rotationAngle: 0)
         self.view.addSubview(theView)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (timer) in
+            self.rotateView(targetView: self.theView)
+        })
+    }
+    
+    func rotateView(targetView:UIView){
+        print("test")
     }
 
 
